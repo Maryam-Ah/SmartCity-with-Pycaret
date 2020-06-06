4be86dc3d4299b640fa980221350f00f33f2453e
Maryam Ahmadi <43885330+Maryam-Ah@users.noreply.github.com>
2020-06-06 13:54:06 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,request, url_for, redirect, render_template, jsonify
+from pycaret.regression import *
+import pandas as pd
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = load_model('finalized_rf_model')
+cols = ['Existing Shelters In Area', 'Max Temp (°C)', 'Min Temp (°C)', 'SECTOR', 'CAPACITY', 'OCCUPANCY_DATE_Month']
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns = cols)
+    prediction = predict_model(model, data=data_unseen, round = 0)
+    prediction = int(prediction.Label[0])
+    return render_template('home.html',pred=‘Number of empty rooms {}'.format(prediction))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
